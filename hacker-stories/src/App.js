869b2f5619facf590ab6a247d220ea6aa65b5738
pedro.abreu <pedro.aurelio.abreu@gmail.com>
2022-10-25 11:26:57 -0300

    modify: included props in components

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
-const list = [
+function App(){
+  const stories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -18,36 +19,54 @@ const list = [
       objectID: 1,
     },
   ];
-
-function App(){
+ 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text"/>      
-    
+      <Search />
       <hr />
-
-      <ul>
-        {list.map(function (item){
-          return (
-          <li key={item.objectID}>
-            <span>
-            <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-          );          
-        })}
-      </ul>
+      <List list={stories} />
 
     </div>
-
-
   );
 }
+
+const Search = ()=> {
+
+  const handleChange = (event) =>{
+    console.log(event.target.value);
+  }
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange}/>   
+    </div>
+  )
+}
+
+
+const List = (props) =>(
+    <ul>
+    {props.list.map(function (item){
+        return (
+          <Item key={item.objectID} item = {item} />
+      );          
+    })}
+    </ul>
+  );
+
+
+const Item = (props) => (
+    <li>
+    <span>
+    <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+    </li>
+  )
 
 export default App;
